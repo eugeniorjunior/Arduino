c7c4e95d19e100c2c082399ff497aea6f0a93bef
eugeniorjunior <54222646+eugeniorjunior@users.noreply.github.com>
2019-08-20 22:17:59 -0300

    adicionei arq via upload da superMaquina

---
+++ superMaquina/superMaquina.ino
@@ -0,0 +1,13 @@
+/**
+ * Sequencial de LEDs  (efeito Supermáquina)
+ * @Author Eugenio Ramos Junior 
+ */
+ 
+
+ void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH); 
+}
